98b204ddce23219782a123578ffdd8afacca1531
Kamil Sławęta <kamilslaweta@gmail.com>
2018-02-09 17:16:08 +0100

    work event

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import style from './Tile.css';
 
-const Tile = props => <input className={style.Tile} type="number" min="1" max="9" value={props.value} step="1"/>;
+const Tile = props => <input className={style.Tile} type="number" min="1" max="9" onChange={props.onChange} value={props.value} step="1"/>;
 
 export default Tile;
